90a00d44fe23d0eeb297e6d9cb4524e511637fd8
Adrián Jesús Rodríguez García <ajrodriguez.garcia@atsistemas.com>
2021-09-29 16:16:20 +0200

    added global store and init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnDestroy, OnInit} from '@angular/core';
+import { GlobalStoreService } from './shared/global-store.service';
+import { Subscription } from 'rxjs';
+import { GlobalStoreModel } from './shared/global-store.interface';
+
+import * as appActions from './actions/actions'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'behavior-store';
+
+  public appModel: GlobalStoreModel;
+
+  private _storeSubscription: Subscription
+
+  constructor(private globalStoreService: GlobalStoreService) {
+    this._storeSubscription = this.globalStoreService.subscribe((m: GlobalStoreModel) => this.__onModelUpdated(m))
+  }
+
+  public ngOnInit(): void {
+    this.__onDataLoaded();
+  }
+
+  public ngOnDestroy(): void
+  {
+    this._storeSubscription.unsubscribe();
+  }
+
+  private __onDataLoaded(): void {
+    this.globalStoreService.dispatchAction(appActions.INIT, null)
+  }
+
+
+
+  private __onModelUpdated(model: GlobalStoreModel ): void {
+    if(model !== undefined && model != null) {
+      this.appModel = model;
+    }
+  }
 }
--- src/app/shared/global-store.service.ts
+++ src/app/shared/global-store.service.ts
@@ -1,19 +1,42 @@
 import { Injectable } from '@angular/core';
+import { GlobalStoreModel, INIT_GLOBAL_STORE } from './global-store.interface';
 
-import {
-  BehaviorSubject,
-  Subscription
-} from 'rxjs';
+import * as AppActions from '../actions/actions';
 
-@Injectable({providedIn: 'root'})
-export class ServiceNameService {
+import { BehaviorSubject, Subscription } from 'rxjs';
 
-  protected subject: BehaviorSubject<any>;
-
-  // protected model: IAppModel;
+@Injectable({ providedIn: 'root' })
+export class GlobalStoreService {
+  protected subject: BehaviorSubject<GlobalStoreModel>;
+  protected model: GlobalStoreModel;
 
   constructor() {
-    this.subject = new BehaviorSubject<any>(false);
+    this.model = JSON.parse(JSON.stringify(INIT_GLOBAL_STORE));
+    this.subject = new BehaviorSubject<GlobalStoreModel>(this.model);
   }
 
+  public subscribe(callback: (model: GlobalStoreModel) => void): Subscription {
+    return this.subject.subscribe(callback);
+  }
+
+  public dispatchAction(act: string, payload: any | null): void {
+    this.model.action = act;
+    this.__updateHistoric();
+    switch (act) {
+      case AppActions.NONE:
+        console.log('no action');
+        break;
+      case AppActions.INIT:
+        console.log('App init', this.model);
+        break;
+      case AppActions.INIT_ERROR:
+        console.log('App init error');
+        break;
+    }
+  }
+
+  private __updateHistoric(): void {
+    const { historic, ...model } = this.model;
+    this.model.historic = [...this.model.historic, model];
+  }
 }
